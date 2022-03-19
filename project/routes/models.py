1d1d498618d19cdcc6c9d4c9a46dd79897e40b55
omerwyo <omerbaggia123@gmail.com>
2022-03-19 12:38:43 +0800

    Product class with a few attr

--- project/routes/models.py
+++ project/routes/models.py
@@ -15,24 +15,30 @@ def dump_datetime(value):
 class Product(db.Model):
     __tablename__='products'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    name = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    registered_on = db.Column(db.DateTime, nullable=False)
-    isVerified = db.Column(db.Boolean, nullable=False, default=False)
+    batchHash = db.Column(db.String, primary_key=True)
+    productName = db.Column(db.String, nullable=False)
+    completed_on = db.Column(db.DateTime, nullable=False)
+    # id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # registered_on = db.Column(db.DateTime, nullable=False)
+    # isVerified = db.Column(db.Boolean, nullable=False, default=False)
 
-    def __init__(self, email, name, password, isVerified=False):
+    def __init__(self, email, name):
         self.email = email
         self.name = name
-        self.password = bcrypt.generate_password_hash(
-            password, app.config.get('BCRYPT_LOG_ROUNDS')
-        ).decode()
-        self.registered_on = datetime.datetime.now()
+        self.completed_on = datetime.datetime.now()
 
     def __repr__(self):
-        return f'User #{self.id}({self.email}, {self.name}'
+        return f'Product #{self.batchHash}({self.productName}, {self.completed_on}'
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'batchHash': self.batchHash,
+            'productName': self.productName,
+            'dateCreated': dump_datetime(self.completed_on),
+        }
+        
     # def encode_auth_token(self, user_id):
     #     '''
     #     Generates the Auth Token
@@ -66,13 +72,4 @@ class Product(db.Model):
     #         return 'Signature expired. Please log in again.'
     #     except jwt.InvalidTokenError:
     #         return 'Invalid token. Please log in again.'
-    
-    @property
-    def serialize(self):
-        """Return object data in easily serializable format"""
-        return {
-            'email': self.email,
-            'name': self.name,
-            'dateCreated': dump_datetime(self.registered_on),
-        }
 # ------------------------------------------------------------------- #
