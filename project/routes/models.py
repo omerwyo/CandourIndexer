268e9d2b695abfc05aff876ee4bc0a02eeebea29
omerwyo <omerbaggia123@gmail.com>
2022-03-21 08:11:06 +0800

    More product clas attr

--- project/routes/models.py
+++ project/routes/models.py
@@ -38,38 +38,5 @@ class Product(db.Model):
             'productName': self.productName,
             'dateCreated': dump_datetime(self.completed_on),
         }
-        
-    # def encode_auth_token(self, user_id):
-    #     '''
-    #     Generates the Auth Token
-    #     :return: string
-    #     '''
-    #     try:
-    #         payload = {
-    #             'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
-    #             'iat': datetime.datetime.utcnow(),
-    #             'sub': user_id
-    #         }
-    #         return jwt.encode(
-    #             payload,
-    #             app.config['SECRET_KEY'],
-    #             algorithm='HS256'
-    #         )
-    #     except Exception as e:
-    #         return e
 
-    # @staticmethod
-    # def decode_auth_token(auth_token):
-    #     """
-    #     Decodes the auth token
-    #     :param auth_token:
-    #     :return: integer|string
-    #     """
-    #     try:
-    #         payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
-    #         return payload['sub']
-    #     except jwt.ExpiredSignatureError:
-    #         return 'Signature expired. Please log in again.'
-    #     except jwt.InvalidTokenError:
-    #         return 'Invalid token. Please log in again.'
 # ------------------------------------------------------------------- #
